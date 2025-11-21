314769bb0da226cadd4fe4cb9c44828f5baa18cd
subhrajit-github <subudhisubhrajit@gmail.com>
2025-11-21 12:01:07 +0530

    commit

---
+++ src/com/String/Pallindrome.java
@@ -0,0 +1,20 @@
+package com.String;
+
+public class Pallindrome {
+
+	public static void main(String[] args) {
+		System.out.println(isPallindrome("madam"));
+	}
+	public static boolean isPallindrome(String s) {
+		int i=0;
+		int j=s.length()-1;
+		while(i<j) {
+			if(s.charAt(i)!=s.charAt(j))
+				return false;
+			i++;
+			j--;
+		}
+		return true;
+	}
+
+}
