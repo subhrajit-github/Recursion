6db4f51d76ddc4b8247bf26b89ad4ea7a470f1dd
subhrajit-github <subudhisubhrajit@gmail.com>
2025-12-01 22:13:02 +0530

    commit

---
+++ src/com/String/PascalsTriangle.java
@@ -0,0 +1,35 @@
+package com.String;
+
+public class PascalsTriangle {
+
+	public static void main(String[] args) {
+		int n=5;
+		int space=n-1;
+		int [][] a=new int[n][];
+		isPascal(a);
+		for(int i=0;i<a.length;i++) {
+			for(int k=0;k<=space;k++) 
+				System.out.print(" ");
+			for(int j=0;j<a[i].length;j++)
+				System.out.print(a[i][j]+" ");
+			System.out.println();
+			space--;
+		}
+		
+		
+	}
+	public static void isPascal(int [][] a) {
+		for(int i=0;i<a.length;i++) {
+			 a[i] =new int[i+1];
+			 for(int j=0;j<a[i].length;j++) {
+				 if(j==0 || i==j) {
+					 a[i][j]=1;
+				 }
+				 else {
+					 a[i][j]=a[i-1][j-1]+a[i-1][j];
+				 }
+			 }
+		}
+	}
+
+}
